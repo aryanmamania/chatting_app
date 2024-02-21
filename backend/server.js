38a8f877d76d1e52b5e4bb46142f4e381a7248ad
mamania0217 <aryanmamania17@gmail.com>
2024-02-21 16:44:09 +0530

    minor fixes

--- backend/server.js
+++ backend/server.js
@@ -16,13 +16,14 @@ app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
 
+
 const __dirname1 = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+  app.use(express.static(path.join(__dirname1, "../frontend/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+    res.sendFile(path.join(__dirname1, "../frontend/build", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
